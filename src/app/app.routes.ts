0f6f02f8e1b0074c283f4aba5be272065fd9b9ac
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-19 12:52:13 +0100

    cargamos rutas y ponemos la maquetacion inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { Error404Component } from './pages/error404/error404.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginFormComponent } from './pages/login-form/login-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", pathMatch: "full", redirectTo: 'home'}, 
+    {path: "home", component: HomeComponent}, 
+    {path: "login", component: LoginFormComponent}, 
+    {path: "dashboard", component: DashboardComponent}, 
+    {path: "**", component: Error404Component}, 
+
+];
