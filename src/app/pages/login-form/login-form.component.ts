695023dbe8a54b61aaf6efc58073b7ff9c136b14
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-21 16:36:01 +0100

    Hacemos el formulario del login con un templatey la funcion que lo recoge

--- src/app/pages/login-form/login-form.component.ts
+++ src/app/pages/login-form/login-form.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { IUser } from '../../interfaces/iuser.interface';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
 
+  getDataForm(loginForm: NgForm){
+    let loginUser: IUser = loginForm.value; 
+    console.log(loginUser); 
+  }
 }
