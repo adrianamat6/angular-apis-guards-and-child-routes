cd7a57d32583433f61d013ce2751ca1cf076c5ac
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-21 17:16:44 +0100

    Gestionamos los mensajes de login correcto e incorrecto con la nueva libreria

--- src/app/pages/login-form/login-form.component.ts
+++ src/app/pages/login-form/login-form.component.ts
@@ -2,6 +2,10 @@ import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { IUser } from '../../interfaces/iuser.interface';
 import { UsersServices } from '../../services/users.services';
+import { toast } from 'ngx-sonner';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login-form',
@@ -12,14 +16,18 @@ import { UsersServices } from '../../services/users.services';
 export class LoginFormComponent {
 
   userService = inject(UsersServices);
+  router = inject(Router); 
 
   async getDataForm(loginForm: NgForm){
     let loginUser: IUser = loginForm.value; 
     try{
-      let response = await this.userService.login(loginUser); 
-      console.log(response)
+      let response:any = await this.userService.login(loginUser); 
+      if (response.success){
+        toast.success(response.success)
+        this.router.navigate(['/dashboard'])
+      }
     }catch(data:any){
-      console.log(data.error.error); 
+      toast.error(data.error.error)
     }
   }
 }
