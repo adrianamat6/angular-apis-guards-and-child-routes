cb69c4683b5386461d3ea3017d4bcba64499d77f
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-22 14:19:26 +0100

    Creamos en el servicio de empleados un método de llamada a la API para obtener los empleados con la cabecera (getHeaders). Esto es el metodo

--- src/app/services/employees.services.ts
+++ src/app/services/employees.services.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IEmployee } from '../interfaces/iemployee.interface';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeesServices {
+  private baseUrl: string = "https://crm-empleados.onrender.com/api/empleados"; 
+  private httpClient =  inject(HttpClient); 
+
+  getAll(): Promise<IEmployee[]>{
+    return lastValueFrom(this.httpClient.get<IEmployee[]>(this.baseUrl, this.getHeaders()))
+  }
   
+  getHeaders(){
+    const httpOptions = {
+      headers: new HttpHeaders({
+       'Content-type': 'application/json',
+      'Authorization': localStorage.getItem('token') || ""
+       })
+    }
+    return httpOptions
+  }
 }
