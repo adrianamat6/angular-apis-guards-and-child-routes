790952c35b16477a35dc6f22001c12fea539b151
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-22 13:50:46 +0100

    Para tener rutas hijas metemos tenemos que poner el <router-outlet> dentro del dashboard.html (eso indica que va a haber rutas hijas). Por otra parte configuramos las rutas hijas en app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,20 @@ import { Error404Component } from './pages/error404/error404.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginFormComponent } from './pages/login-form/login-form.component';
 import { loginGuard } from './guards/login-guard';
+import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './pages/employee-form/employee-form.component';
 
 export const routes: Routes = [
     {path: "", pathMatch: "full", redirectTo: 'home'}, 
     {path: "home", component: HomeComponent}, 
     {path: "login", component: LoginFormComponent}, 
-    {path: "dashboard", component: DashboardComponent, canActivate:[loginGuard]}, 
+    { path: "dashboard", component: DashboardComponent, canActivate: [loginGuard], 
+        children: [ 
+                { path: "empleados", component: EmployeeListComponent }, 
+                { path: "nuevo-empleado", component: EmployeeFormComponent } ,
+                { path: "actualizar-empleado/:id", component: EmployeeFormComponent } 
+                  ] 
+        }, 
     {path: "**", component: Error404Component}, 
 
 ];
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from '../../shared/nav/nav.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [NavComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
