da21e461cc2e5773b883e5f61f411b69e3e8b94f
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-22 13:29:12 +0100

    A la guarda bloquea el dashboad si no hay token almacenado en el localStorage. En casi de que si exista, deja paso. Para guardar el token,  localStorage.setItem en login-form.component.ts

--- src/app/guards/login-guard.ts
+++ src/app/guards/login-guard.ts
@@ -1,5 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const loginGuard: CanActivateFn = (route, state) => {
+
+  const router = inject(Router); 
+  // si existe token=true, sino false
+    if (localStorage.getItem('token')){
+      return true; 
+    }
+
+  router.navigate(['/login']); 
   return false;
 };
