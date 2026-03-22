c95cc2e7a4fe3f30bd430b231812e1731d696374
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2026-03-22 14:24:54 +0100

    Añadimos metodo en employee-list.ts para llamar al servicio y traernos a todos los empleados con el getAll.

--- src/app/pages/employee-list/employee-list.component.ts
+++ src/app/pages/employee-list/employee-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IEmployee } from '../../interfaces/iemployee.interface';
+import { EmployeesServices } from '../../services/employees.services';
 
 @Component({
   selector: 'app-employee-list',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './employee-list.component.css',
 })
 export class EmployeeListComponent {
+  arrEmployees: IEmployee[] = []; 
+  employeesServices = inject(EmployeesServices); 
 
+  ngOnInit(){
+    this.cargarEmpleados()
+  }
+
+  async cargarEmpleados(){
+
+    try{
+      this.arrEmployees = await this.employeesServices.getAll(); 
+      console.log(this.arrEmployees); 
+    }catch(error){
+      console.log(error); 
+    }
+
+  }
 }
+
+
